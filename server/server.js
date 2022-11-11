514ff7199f97c337eb957d633af713daa604171b
mitalrs <mitalsapkale1999@gmail.com>
2022-11-12 00:38:59 +0530

    add the npm packages nodemon,cros

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+
 
 const PORT = 4000
 const app = express();
+
+app.use(cors)
 
 await mongoose.connect("mongodb+srv://mital:Mitalmital@cluster0.uqsmrq1.mongodb.net/?retryWrites=true&w=majority")
  console.log('MongoDB connection is successful');
