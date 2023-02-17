9ab59d80df73ad94126e959157999ee8b2835f06
mitalrs <mitalsapkale1999@gmail.com>
2023-02-17 23:19:36 +0530

    solve the error facing in transactionForm file on the update time

--- clinet/src/components/TransactionForm.js
+++ clinet/src/components/TransactionForm.js
@@ -8,111 +8,112 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
-import {useState} from "react";
-import { create } from 'domain';
+import { useState } from "react";
+// import { create } from 'domain';
 
 const InitialForm = {
-  amount:0,
-  description:'',
+  amount: 0,
+  description: '',
   date: new Date(),
 };
 
-export default function TransactionForm({fetchTransactions, editTransaction}) {
+export default function TransactionForm({ fetchTransactions, editTransaction }) {
   const [form, setForm] = useState(InitialForm);
 
-  React.useEffect(()=>{
-    if(editTransaction !== {}){
+  React.useEffect(() => {
+    if (editTransaction !== {}) {
       setForm(editTransaction);
     }
   }, [editTransaction]);
-  
-  function handleChange(e){
-    setForm({ ... form, [e.target.name]:e.target.value})
+
+  function handleChange(e) {
+    setForm({ ...form, [e.target.name]: e.target.value })
   }
 
-  function handleDate(newValue){
-    setForm({...form, date:newValue})
+  function handleDate(newValue) {
+    setForm({ ...form, date: newValue })
   }
 
-  async function handleSubmit(e){
+  async function handleSubmit(e) {
     e.preventDefault();
-    const res = editTransaction === {} ? create(): update()
-    }
+    const res = editTransaction === {} ? create() : update();
+  
 
-    if(res.ok){
-      setForm(InitialForm);
-      fetchTransactions();
-    }
+  
+  if (res.ok) {
+    setForm(InitialForm);
+    fetchTransactions();
   }
+}
 
-  async function create(){
-    const res = await fetch('http://localhost:4000/transaction',{
-      method: "POST",
-      body: JSON.stringify(form),
-      headers:{
-        'content-type': "application/json",
-      }
-    });
-    return res;
-  }
+async function create() {
+  const res = await fetch('http://localhost:4000/transaction', {
+    method: "POST",
+    body: JSON.stringify(form),
+    headers: {
+      'content-type': "application/json",
+    },
+  });
+  return res;
+}
 
-  async function update(){
-    const res = await fetch(`http://localhost:4000/transaction/${editTransaction._id}`,
+async function update() {
+  const res = await fetch(`http://localhost:4000/transaction/${editTransaction._id}`,
     {
       method: "PATCH",
       body: JSON.stringify(form),
-      headers:{
+      headers: {
         'content-type': "application/json",
       }
     });
-    return res;
-  }
+  return res;
+}
 
-  return (
-    <Card sx={{ minWidth: 275, marginTop:10 }}>
-      <CardContent>
+return (
+  <Card sx={{ minWidth: 275, marginTop: 10 }}>
+    <CardContent>
       <Typography variant="h6">Add new transaction</Typography>
-        
-        <form onSubmit={handleSubmit}>
-        <TextField 
-        sx={{marginRight: 5}} 
-        id="outlined-basic" 
-        label="Amount" 
-        size="small" 
-        name="amount"
-        variant="outlined" 
-        value={form.amount} 
-        onChange={handleChange}
+
+      <form onSubmit={handleSubmit}>
+        <TextField
+          sx={{ marginRight: 5 }}
+          id="outlined-basic"
+          label="Amount"
+          size="small"
+          name="amount"
+          variant="outlined"
+          value={form.amount}
+          onChange={handleChange}
         />
-        <TextField 
-        sx={{marginRight: 5}} 
-        id="outlined-basic" 
-        label="Description" 
-        size="small" 
-        name="description"
-        variant="outlined" 
-        value={form.description} 
-        onChange={handleChange}
+        <TextField
+          sx={{ marginRight: 5 }}
+          id="outlined-basic"
+          label="Description"
+          size="small"
+          name="description"
+          variant="outlined"
+          value={form.description}
+          onChange={handleChange}
         />
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DesktopDatePicker
-          label="Transaction Date"
-          inputFormat="MM/DD/YYYY"
-          value={form.date}
-          onChange={handleDate}
-          renderInput={(params) => <TextField sx={{marginRight: 5}} size="small" {...params} />}
-        />
-         </LocalizationProvider>
-         {editTransaction !== {} && (
+          <DesktopDatePicker
+            label="Transaction Date"
+            inputFormat="MM/DD/YYYY"
+            value={form.date}
+            onChange={handleDate}
+            renderInput={(params) => <TextField sx={{ marginRight: 5 }} size="small" {...params} />}
+          />
+        </LocalizationProvider>
+        {editTransaction !== {} && (
           <Button type="submit" variant="secondary">Update</Button>
-         )}
-         {editTransaction === {} && (
+        )}
+        {editTransaction === {} && (
           <Button type="submit" variant="contained">Submit</Button>
-         )}
-         
-        </form>
-        
-      </CardContent>
-    </Card>
-  );
+        )}
+
+      </form>
+
+    </CardContent>
+  </Card>
+);
 }
