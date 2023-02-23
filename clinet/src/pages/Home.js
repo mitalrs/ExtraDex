6bc873e2dc00d968cacce1d2f2f1d11a617b9d32
mitalrs <mitalsapkale1999@gmail.com>
2023-02-23 16:02:54 +0530

    solved the error thats unauthorized user and fetching transaction from transaction model

--- clinet/src/pages/Home.js
+++ clinet/src/pages/Home.js
@@ -17,7 +17,6 @@ export default function Home() {
 
     async function fetchTransactions() {
         const token = Cookies.get('token');
-        console.log(token)
         const res = await fetch('http://localhost:4000/transaction', 
         {
             headers: {
@@ -25,7 +24,6 @@ export default function Home() {
             },
         });
         const { data } = await res.json();
-        console.log(data);
         // console.log(data);
         setTransactions(data);
     }
