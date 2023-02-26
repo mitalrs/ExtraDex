7f47bda7b544f44dd83aa466f4966d7480ecec51
mitalrs <mitalsapkale1999@gmail.com>
2023-02-26 17:40:43 +0530

    done logout dispatch

--- clinet/src/store/auth.js
+++ clinet/src/store/auth.js
@@ -11,9 +11,13 @@ export const authSlice = createSlice({
       state.user = payload.user;
       state.isAuthenticated = true;
     },
+    logout: (state) => {
+      state.user = {}
+      state.isAuthenticated = false;
+    }
   },
 });
 
-export const { getUser } = authSlice.actions;
+export const { getUser, logout } = authSlice.actions;
 
 export default authSlice.reducer
