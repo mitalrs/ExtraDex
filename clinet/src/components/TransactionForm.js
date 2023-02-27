1eff263aa1f9d5a58fd3ab4ef5b386eb6e940378
mitalrs <mitalsapkale1999@gmail.com>
2023-02-28 00:35:03 +0530

    added catogery

--- clinet/src/components/TransactionForm.js
+++ clinet/src/components/TransactionForm.js
@@ -8,19 +8,27 @@ import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 import Cookies from 'js-cookie';
-
+import Autocomplete from '@mui/material/Autocomplete';
 import { useState } from "react";
+import Box from '@mui/material/Box';
 // import { create } from 'domain';
 
 const InitialForm = {
   amount: 0,
   description: '',
   date: new Date(),
+  category: ''
 };
 
 export default function TransactionForm({ fetchTransactions, editTransaction }) {
   const token = Cookies.get('token');
   const [form, setForm] = useState(InitialForm);
+  const categories = [
+    { label: 'Travel' },
+    { label: 'Shopping' },
+    { label: 'Investment' },
+    { label: 'Bills' },
+  ]
 
   React.useEffect(() => {
     if (editTransaction.amount !== undefined) {
@@ -42,83 +50,92 @@ export default function TransactionForm({ fetchTransactions, editTransaction }) 
 
   }
 
-  function reload(res){
+  function reload(res) {
     if (res.ok) {
       setForm(InitialForm);
       fetchTransactions();
     }
   }
 
-async function create() {
-  const res = await fetch(`${process.env.REACT_APP_API_URL}/transaction`, {
-    method: "POST",
-    body: JSON.stringify(form),
-    headers: {
-      'content-type': "application/json",
-      'Authorization': `Bearer ${token}`,
-    },
-  });
-  reload(res);
-}
-
-async function update() {
-  const res = await fetch(`${process.env.REACT_APP_API_URL}/transaction/${editTransaction._id}`,
-    {
-      method: "PATCH",
+  async function create() {
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/transaction`, {
+      method: "POST",
       body: JSON.stringify(form),
       headers: {
         'content-type': "application/json",
         'Authorization': `Bearer ${token}`,
-      }
+      },
     });
     reload(res);
-}
+  }
 
-return (
-  <Card sx={{ minWidth: 275, marginTop: 10 }}>
-    <CardContent>
-      <Typography variant="h6">Add new transaction</Typography>
+  async function update() {
+    const res = await fetch(`${process.env.REACT_APP_API_URL}/transaction/${editTransaction._id}`,
+      {
+        method: "PATCH",
+        body: JSON.stringify(form),
+        headers: {
+          'content-type': "application/json",
+          'Authorization': `Bearer ${token}`,
+        }
+      });
+    reload(res);
+  }
 
-      <form onSubmit={handleSubmit}>
-        <TextField
-          sx={{ marginRight: 5 }}
-          id="outlined-basic"
-          label="Amount"
-          size="small"
-          name="amount"
-          variant="outlined"
-          value={form.amount}
-          onChange={handleChange}
-        />
-        <TextField
-          sx={{ marginRight: 5 }}
-          id="outlined-basic"
-          label="Description"
-          size="small"
-          name="description"
-          variant="outlined"
-          value={form.description}
-          onChange={handleChange}
-        />
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-          <DesktopDatePicker
-            label="Transaction Date"
-            inputFormat="MM/DD/YYYY"
-            value={form.date}
-            onChange={handleDate}
-            renderInput={(params) => <TextField sx={{ marginRight: 5 }} size="small" {...params} />}
+  return (
+    <Card sx={{ minWidth: 275, marginTop: 10 }}>
+      <CardContent>
+        <Typography variant="h6">Add new transaction</Typography>
+
+        <Box component='form' onSubmit={handleSubmit} sx={{display: "flex"}}>
+          <TextField
+            sx={{ marginRight: 5 }}
+            id="outlined-basic"
+            label="Amount"
+            size="small"
+            name="amount"
+            variant="outlined"
+            value={form.amount}
+            onChange={handleChange}
           />
-        </LocalizationProvider>
-        {editTransaction.amount !== undefined && (
-          <Button type="submit" variant="secondary">Update</Button>
-        )}
-        {editTransaction.amount === undefined && (
-          <Button type="submit" variant="contained">Submit</Button>
-        )}
+          <TextField
+            sx={{ marginRight: 5 }}
+            id="outlined-basic"
+            label="Description"
+            size="small"
+            name="description"
+            variant="outlined"
+            value={form.description}
+            onChange={handleChange}
+          />
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DesktopDatePicker
+              label="Transaction Date"
+              inputFormat="MM/DD/YYYY"
+              value={form.date}
+              onChange={handleDate}
+              renderInput={(params) => <TextField sx={{ marginRight: 5 }} size="small" {...params} />}
+            />
+          </LocalizationProvider>
 
-      </form>
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={categories}
+            sx={{ width: 200, marginRight: 5 }}
+            renderInput={(params) => <TextField {...params} size="small" label="Category" />}
+          />
 
-    </CardContent>
-  </Card>
-);
+          {editTransaction.amount !== undefined && (
+            <Button type="submit" variant="secondary">Update</Button>
+          )}
+          {editTransaction.amount === undefined && (
+            <Button type="submit" variant="contained">Submit</Button>
+          )}
+
+        </Box>
+
+      </CardContent>
+    </Card>
+  );
 }
