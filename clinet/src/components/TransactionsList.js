fd2290c8f80cc4c0b83d05836fa90233cb4d1131
mitalrs <mitalsapkale1999@gmail.com>
2023-02-27 23:53:56 +0530

    transaction of users

--- clinet/src/components/TransactionsList.js
+++ clinet/src/components/TransactionsList.js
@@ -11,15 +11,20 @@ import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
 import DeleteSharpIcon from '@mui/icons-material/DeleteSharp';
 import IconButton from '@mui/material/IconButton';
 import dayjs from 'dayjs';
+import Cookies from 'js-cookie';
 
 
 
 export default function TransactionsList({ transactions, fetchTransactions, setEditTransaction }) {
 
   async function remove(_id) {
+    const token = Cookies.get('token');
     if (!window.confirm('Are you sure to delete')) return;
     const res = await fetch(`${process.env.REACT_APP_API_URL}/transaction/${_id}`, {
       method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
     });
     if (res.ok) {
       fetchTransactions();
--- server/controller/TransactionController.js
+++ server/controller/TransactionController.js
@@ -2,17 +2,17 @@ import Transaction from "../models/Transaction.js";
 
 
 export const index = async (req, res) => {
-    const transaction = await Transaction.find({}).sort({ createdAt: -1 });
+    const transaction = await Transaction.find({user_id: req.user._id}).sort({ createdAt: -1 });
     res.json({ data: transaction });
 };
 
 export const create = async (req, res) => {
-    console.log(req.user);
     const { amount, description, date } = req.body;
     const transaction = new Transaction({
         amount,
         description,
         date,
+        user_id: req.user._id,
     });
     await transaction.save();
     res.json({ message: "success" });
