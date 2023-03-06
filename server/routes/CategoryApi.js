042aef95c21d5ab6dbf089237bc5604306c9f460
mitalrs <mitalsapkale1999@gmail.com>
2023-03-06 12:43:49 +0530

    create category on server side

--- server/routes/CategoryApi.js
+++ server/routes/CategoryApi.js
@@ -3,7 +3,7 @@ import * as CategoryController from '../controller/CategoryController.js';
 
 const router = Router();
 
-router.delete('/:id', CategoryController.destroy 
-);
+router.delete('/:id', CategoryController.destroy );
+router.post("/", CategoryController.create);
 
 export default router;
