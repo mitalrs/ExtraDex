9880f2ab157c8ba0a1817c22b3dba2edb6c88d46
mitalrs <mitalsapkale1999@gmail.com>
2023-03-09 12:48:26 +0530

    done

--- clinet/src/components/TransactionChart.js
+++ clinet/src/components/TransactionChart.js
@@ -14,25 +14,18 @@ import { scaleBand } from '@devexpress/dx-chart-core';
 import { ArgumentScale } from '@devexpress/dx-react-chart';
 import { Animation } from '@devexpress/dx-react-chart';
 import { EventTracker } from '@devexpress/dx-react-chart';
-
-// const data = [
-//   { year: '1950', population: 2.525 },
-//   { year: '1960', population: 3.018 },
-//   { year: '1970', population: 3.682 },
-//   { year: '1980', population: 4.440 },
-//   { year: '1990', population: 5.310 },
-//   { year: '2000', population: 6.127 },
-//   { year: '2010', population: 6.930 },
-// ];
-
-
+import dayjs from 'dayjs'
 
 export default function TransactionChart({data}) {
+  const chartDate = data.map(item => {
+    item.month = dayjs().month(item._id-1).format('MMMM');
+    return item
+  })
   
   return (
     <Paper sx={{marginTop:5}}>
       <Chart
-        data={data}
+        data={chartDate}
       >
        <ArgumentScale factory={scaleBand} />
         <ArgumentAxis />
@@ -40,7 +33,7 @@ export default function TransactionChart({data}) {
 
         <BarSeries
           valueField="totalExpenses"
-          argumentField="_id"
+          argumentField="month"
         />
          <Animation />
          <EventTracker />
